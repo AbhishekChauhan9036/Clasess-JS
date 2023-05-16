6d5994a36b7f0612ff74a16cca9ba62f82da42ee
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-16 19:57:52 +0530

    Lecture 1

---
+++ 1stDay.js
@@ -0,0 +1,20 @@
+//function Definition
+function SayHello(name) {
+    return name;
+}
+
+
+var SayHello1 = function () {
+    console.log("Hello Ram!");
+}
+
+//function Call
+let Fname="Satish";
+let Lname="Gupta";
+let marks=String(123);
+let t1=190.897;
+
+let ans=Fname+marks;
+console.log(typeof marks);
+console.log(parseFloat(t1))
+console.log(ans)
