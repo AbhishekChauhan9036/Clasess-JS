e2828a8034ac57b3573327edb69e8edc5b69bdca
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-16 20:15:25 +0530

    Lecture 1

--- 1stDay.js
+++ 1stDay.js
@@ -18,3 +18,20 @@ let ans=Fname+marks;
 console.log(typeof marks);
 console.log(parseFloat(t1))
 console.log(ans)
+
+
+
+
+
+//  Revere a string
+function reverseString(str) {
+    let temp = "";
+    for (let i = 0; i < str.length; i++) {
+        temp += str[str.length - i - 1];
+    }
+    return temp;
+}
+
+let Fname = "Satish";
+let ans = reverseString(Fname);
+console.log(ans);
