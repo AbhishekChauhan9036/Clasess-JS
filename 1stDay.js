1142d491d05a7f0e0fcecc327e9e526820794457
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-16 20:37:39 +0530

    Lecture 1

--- 1stDay.js
+++ 1stDay.js
@@ -35,3 +35,24 @@ function reverseString(str) {
 let Fname = "Satish";
 let ans = reverseString(Fname);
 console.log(ans);
+
+
+
+
+
+//Search a element and return index of the element
+function Solve(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+// const arr = [12, "apple", 12.098, "wood", "Mohan", true, false];r
+const brr = [10, 20, 51, 90, 54, 89, 32];
+const target = 42;
+let ans = Solve(brr, target);
+console.log(ans);
