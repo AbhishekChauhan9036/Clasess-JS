7938c7a32325f3a37ac22f1507cbdc669247d6b1
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-18 21:06:17 +0530

    JS classes

--- 3rdDay.js
+++ 3rdDay.js
@@ -32,3 +32,28 @@ q=q^r;
 r=q^r;
 q=q^r;
 console.log(q,r);
+
+
+
+
+
+
+
+
+
+/**
+1929. Concatenation of Array
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    
+    const ans=new Array(nums.length*2);
+    for(let i=0;i<nums.length;i++){
+        ans[i] = nums[i];
+        ans[i+nums.length] = nums[i];
+    }
+    return ans;
+};
+
+
