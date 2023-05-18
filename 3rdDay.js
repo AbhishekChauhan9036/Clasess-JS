48dd50a48b5ae6b441a453eb42f76d154d77bad3
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-18 20:43:29 +0530

    JS classes

---
+++ 3rdDay.js
@@ -0,0 +1,32 @@
+// Swap two Number 
+let a=10;
+let b=20;
+let temp=a;
+a=b;
+b=temp;
+console.log(a,b);
+
+
+
+let x=10;
+let y=20;
+[[x,y]=[y,x]]
+console.log(x,y);
+
+
+
+let m=10;
+let n=20;
+m=m+n;// 10+20
+n=m-n;// 30-20
+m=m-n;//30-10
+console.log(m,n);
+
+
+
+let q=10;
+let r=20;
+q=q^r;
+r=q^r;
+q=q^r;
+console.log(q,r);
